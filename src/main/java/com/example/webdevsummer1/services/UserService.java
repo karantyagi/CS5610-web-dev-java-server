e72bc54d6d8b2e830845237e8a23d86927b399a9
karantyagi <tyagi.k@husky.neu.edu>
2018-05-13 18:11:40 -0400

    creating user admin service

---
+++ src/main/java/com/example/webdevsummer1/services/UserService.java
@@ -0,0 +1,22 @@
+package com.example.webdevsummer1.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdevsummer1.models.User;
+import com.example.webdevsummer1.repositories.UserRepository;
+
+@RestController
+public class UserService {
+	@Autowired
+	UserRepository repository;
+	
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>) repository.findAll();
+	}
+	
+}
