4c76bdcb65712e6268a246dd7f86b22d2a30e78d
karantyagi <tyagi.k@husky.neu.edu>
2018-05-15 19:38:21 -0400

    added form validations to login

--- src/main/webapp/jquery/components/login/login.controller.client.js
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -1,18 +1,56 @@
 (function () {
     var $usernameFld, $passwordFld;
     var $loginBtn;
-    var userService = new AdminUserServiceClient();
+    var userService = new UserServiceClient();
     $(main);
 
     function main(){
-    	
-    
+    	$('#loginBtn').click(login);
     } // end of main function
     
     function login(){
+    	console.log("Login button clicked");
+    	if(validate()){
+    		var user = {
+					username: $('#usernameFld').val(),
+					password: $('#passwordFld').val()};
+    	console.log(user);	
+    	validateUser(user);
+    	   
+    	}
     	
     } // end of login function
     
+   
+    function validateUser(){
+    	
+    	console.log("user find user by credentials...coming up...")
+    }
     
     
+    
+    function validate(){
+		// read values from UI
+    	var username = $('#usernameFld').val();
+		var password = $('#passwordFld').val();
+		
+		console.log("inside validations");
+		
+		// Validations
+		
+		if(username == ""){
+			alert("Please fill username!");
+			return false;
+		}
+		
+		if(password == ""){
+			alert("Please fill password!");
+			return false;
+		}
+		
+		return true;
+		
+	} // end of validate function 
+    
+
 })();
