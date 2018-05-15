2cce6576283b4aaf0524d72da33e61da581c9e6d
karantyagi <tyagi.k@husky.neu.edu>
2018-05-15 19:25:12 -0400

    fixed login user interface

---
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -0,0 +1,18 @@
+(function () {
+    var $usernameFld, $passwordFld;
+    var $loginBtn;
+    var userService = new AdminUserServiceClient();
+    $(main);
+
+    function main(){
+    	
+    
+    } // end of main function
+    
+    function login(){
+    	
+    } // end of login function
+    
+    
+    
+})();
