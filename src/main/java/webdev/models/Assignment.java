bcdf388b444ed3a3eaf4dfd2a9b2a0e17459cadb
karantyagi <tyagi.k@husky.neu.edu>
2018-06-05 09:15:56 -0400

    added assignment model and CRUD repository

---
+++ src/main/java/webdev/models/Assignment.java
@@ -0,0 +1,59 @@
+package webdev.models;
+
+import javax.persistence.Entity;
+
+
+
+@Entity
+public class Assignment extends Widget {
+	
+	private String title;
+	private String subtitle;
+	private String description;
+	private int points;
+	private String essayAnswer;
+	private String filename;
+	private String link;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSubtitle() {
+		return subtitle;
+	}
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public String getEssayAnswer() {
+		return essayAnswer;
+	}
+	public void setEssayAnswer(String essayAnswer) {
+		this.essayAnswer = essayAnswer;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+}
