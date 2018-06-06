e08cab45c2a8670e9d147de6af5ddf14551944e2
karantyagi <tyagi.k@husky.neu.edu>
2018-06-06 01:30:23 -0400

    added delete question by id to question services

--- src/main/java/webdev/services/QuestionService.java
+++ src/main/java/webdev/services/QuestionService.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -153,6 +154,14 @@ public class QuestionService {
 		Exam exam = data.get();
 		que.setExam(exam);
 		return fillRepo.save(que);
+	}
+	
+	
+	@DeleteMapping("/api/question/{questionId}")
+	public void deleteQuestionById(@PathVariable("questionId") int qId)
+	{
+//		widgetRepository.deleteById(qId);
+		baseRepo.deleteById(qId);
 	}
 
 		
